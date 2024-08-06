d804ac76f9a1b157a2ed52531c219da825cdc202
Sasha <st087955@student.spbu.ru>
2024-08-06 14:57:14 +0300

    add getHumanChoice function
    getHumanChoice return one of the valid choices depending on what the
    user inputs by using prompt. Trim extra spaces and make all letters
    lowercase in the entered line.

--- javascript.js
+++ javascript.js
@@ -2,3 +2,15 @@ function getComputerChoice() {
     let options = ['камень', 'ножницы', 'бумага'];
     return options[Math.floor(Math.random() * 3)]
 }
+
+function getHumanChoice() {
+    let answer
+    while (1) {
+        answer = prompt('Выберите "камень", "ножницы" или "бумагу"');
+        answer = answer.toLowerCase().trim();
+        if (answer === "камень" || answer ===  "ножницы" || answer === "бумага") {
+            break
+        }
+    }
+    return answer
+}
