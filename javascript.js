4713e9bb9bf2883d1f2d47aeae7d7a97629cca24
Sasha <st087955@student.spbu.ru>
2024-08-06 16:27:51 +0300

    add playGame function
    Move playRound function and score variables so that they're declared
    inside of the new playGame function. playGame function calls playRound
    to play 5 rounds, keeps track of the scores and declares a winner at the
    end.

--- javascript.js
+++ javascript.js
@@ -15,28 +15,45 @@ function getHumanChoice() {
     return answer
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log(`Ничья! Вы и компьютер выбросили ${humanChoice}.`);
-        humanScore += 1;
-        computerScore += 1;
-    }
-    else if (humanChoice === 'камень' && computerChoice === 'ножницы'
-            || humanChoice === 'ножницы' && computerChoice === 'бумага'
-            || humanChoice === 'бумага' && computerChoice === 'камень' ) {
-                console.log(`Вы победили! ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} бьёт ${computerChoice}.`);
-                humanScore += 1;
-            }
-    else {
-        console.log(`Вы проиграли! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} бьёт ${humanChoice}.`);
+function playGame() {
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log(`Ничья! Вы и компьютер выбросили ${humanChoice}.`);
+            humanScore += 1;
             computerScore += 1;
-    };
+        }
+        else if (humanChoice === 'камень' && computerChoice === 'ножницы'
+                || humanChoice === 'ножницы' && computerChoice === 'бумага'
+                || humanChoice === 'бумага' && computerChoice === 'камень' ) {
+                    console.log(`Вы победили! ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} бьёт ${computerChoice}.`);
+                    humanScore += 1;
+                }
+        else {
+            console.log(`Вы проиграли! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} бьёт ${humanChoice}.`);
+                computerScore += 1;
+        };
+    }; 
+
+    let humanScore = 0,
+        computerScore = 0;
+
+    for (let i = 0; i < 5; i++){
+        let humanChoice = getHumanChoice(),
+            computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+
+    console.log('Игра окончена!');
+    if (humanScore == computerScore) {
+        console.log('Ничья! Спасибо за Игру!');
+    }
+    else if (humanScore > computerScore) {
+        console.log('Вы победили! Поздравляем!');
+    }
+    else {
+        console.log('Вы проиграли! Попробуйте снова!');
+    }
 }
 
-let humanScore = 0,
-    computerScore = 0;
-
-let humanChoice = getHumanChoice(),
-    computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice);
+playGame();
