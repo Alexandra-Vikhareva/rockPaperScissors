571817539c6bef50b1206bbf6c603a5d6c4f8221
Sasha <st087955@student.spbu.ru>
2024-08-06 15:36:58 +0300

    add playRound function
    playRound function takes the human and computer player choices as
    arguments, plays a single round, increments the round winner`s score
    and logs a winner announcement.

--- javascript.js
+++ javascript.js
@@ -14,3 +14,29 @@ function getHumanChoice() {
     }
     return answer
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Ничья! Вы и компьютер выбросили ${humanChoice}.`);
+        humanScore += 1;
+        computerScore += 1;
+    }
+    else if (humanChoice === 'камень' && computerChoice === 'ножницы'
+            || humanChoice === 'ножницы' && computerChoice === 'бумага'
+            || humanChoice === 'бумага' && computerChoice === 'камень' ) {
+                console.log(`Вы победили! ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} бьёт ${computerChoice}.`);
+                humanScore += 1;
+            }
+    else {
+        console.log(`Вы проиграли! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} бьёт ${humanChoice}.`);
+            computerScore += 1;
+    };
+}
+
+let humanScore = 0,
+    computerScore = 0;
+
+let humanChoice = getHumanChoice(),
+    computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
